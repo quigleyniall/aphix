16acc0b1fbd1f4c19719f84a6f4296969448788a
Niall Quigley <niallquigley92@gmail.com>
2020-04-22 09:59:56 +0100

    Change logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import Logo from '../assets/aphix.png'
 const Nav = () => (
   <div className="nav">
     <img src={Logo} alt="Aphix Software" className="nav__logo" />
-    <h3 className="nav__heading">VAT Calulator</h3>
+    <h3 className="nav__heading">VAT Calculator</h3>
   </div>
 );
 
